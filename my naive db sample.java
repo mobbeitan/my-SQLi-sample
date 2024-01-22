8733a29dad50dee0e7f34c575a17b9d068c460cd
Eitan Worcel <96389636+mobbeitan@users.noreply.github.com>
2024-01-22 13:22:49 -0500

    Update my naive db sample.java

--- my naive db sample.java
+++ my naive db sample.java
@@ -12,9 +12,9 @@ public class SQLInjectionExample extends HttpServlet {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db");
             String user = request.getParameter("username");
 
-            //String query = "SELECT * FROM users WHERE username = '" + user + "';";
-            //Statement stmt = con.createStatement();
-            //stmt.executeQuery(query);
+            String query = "SELECT * FROM users WHERE username = '" + user + "';";
+            Statement stmt = con.createStatement();
+            stmt.executeQuery(query);
         } catch (Exception e) {
             throw new ServletException(e);
         }
